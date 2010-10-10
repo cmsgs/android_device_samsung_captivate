9ae5d49079991f5ceb744c7c22663ead3c9dc13b
Atin Malaviya <atin.malaviya@gmail.com>
2010-10-09 21:50:48 -0400

    Added SDCARD mount points.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -88,11 +88,15 @@ BOARD_SYSTEM_FILESYSTEM_OPTIONS := llw,check=no
 BOARD_CACHE_DEVICE := /dev/block/stl11
 BOARD_CACHE_FILESYSTEM := auto
 BOARD_CACHE_FILESYSTEM_OPTIONS := llw,check=no,nosuid,nodev
-BOARD_SDCARD_DEVICE_PRIMARY := /dev/block/mmcblk0
-BOARD_SDCARD_DEVICE_SECONDARY := /dev/block/mmcblk0p1
-BOARD_SDEXT_DEVICE := /dev/block/mmcblk1
+BOARD_SDCARD_DEVICE_PRIMARY := /dev/block/mmcblk0p1
+#BOARD_SDCARD_DEVICE_SECONDARY := /dev/block/mmcblk1p1
+BOARD_SDCARD_DEVICE_MOUNT := /mnt/sdcard
+BOARD_SDEXT_DEVICE := /dev/block/mmcblk1p1
+BOARD_SDEXT_DEVICE_MOUNT := /mnt/sdcard/external_sd
 #BOARD_USES_BMLUTILS := true
-BOARD_CUSTOM_RECOVERY_KEYMAPPING := true
+#BOARD_CUSTOM_RECOVERY_KEYMAPPING := true
 TARGET_PREBUILT_KERNEL := device/samsung/captivate/kernel
 #BOARD_GPS_LIBRARIES := libgps libsecgps
 BOARD_GPS_LIBRARIES := 
+
+#TARGET_RECOVERY_UI_LIB := librecovery_ui_captivate
