0763b1fdb453f73e846c7418698abc2ac0d281c5
Gary Danko <gdanko@gmail.com>
2010-12-03 13:04:23 -0800

    Added some changes codeworkx has in order to enable some features, stay in line with his stuff, and hopefully stop the bootable recovery errors on compile

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -21,7 +21,9 @@
 # variant, so that it gets overwritten by the parent (which goes
 # against the traditional rules of inheritance).
 
-USE_CAMERA_STUB := true
+#USE_CAMERA_STUB := true
+BOARD_OVERLAY_FORMAT_YCbRc_420_SP := true
+BUILD_PV_VIDEO_ENCODERS := 1
 
 # inherit from the proprietary version
 -include vendor/samsung/captivate/BoardConfigVendor.mk
@@ -38,40 +40,54 @@ TARGET_BOARD_PLATFORM_GPU := POWERVR_SGX540_120
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_GLOBAL_CFLAGS += -mtune=cortex-a8
+TARGET_GLOBAL_CPPFLAGS += -mtune=cortex-a8
+ANDROID_ARM_LINKER := true
 
 # Sound related defines
 BOARD_USES_ALSA_AUDIO := true
 BUILD_WITH_ALSA_UTILS := true
+ALSA_DEFAULT_SAMPLE_RATE := 44100
+
+# For the FM Radio
+BOARD_HAVE_FM_RADIO := true
 
 # WiFi related defines
 BOARD_WPA_SUPPLICANT_DRIVER := WEXT
 WPA_SUPPLICANT_VERSION := VER_0_6_X
-#BOARD_WLAN_DEVICE := bcm4329
+BOARD_WLAN_DEVICE := bcm4329
 WIFI_DRIVER_MODULE_PATH := "/lib/modules/dhd.ko"
-#MFGDRV_MODULE_ARG := "firmware_path=/system/etc/wifi/bcm4329_mfg.bin nvram_path=/system/etc/wifi/nvram_mfg.txt"
-#DRV_AP_MODULE_ARG := "firmware_path=/system/etc/wifi/bcm4329_aps.bin nvram_path=/system/etc/wifi/nvram_net.txt dhd_watchdog_ms=200 dhd_poll=1"
+WIFI_DRIVER_FW_STA_PATH := "/system/etc/wifi/bcm4329_sta.bin"
+WIFI_DRIVER_FW_AP_PATH := "/system/etc/wifi/bcm4329_aps.bin"
 WIFI_DRIVER_MODULE_ARG := "firmware_path=/system/etc/wifi/bcm4329_sta.bin nvram_path=/system/etc/wifi/nvram_net.txt dhd_watchdog_ms=10 dhd_poll=1"
 WIFI_DRIVER_MODULE_NAME := "dhd"
+CONFIG_DRIVER_WEXT := true
 
 # Bluetooth related defines
+BOARD_HAVE_BLUETOOTH_BCM := true
 BOARD_HAVE_BLUETOOTH := true
 BT_USE_BTL_IF := true
 BT_ALT_STACK := true
 BRCM_BTL_INCLUDE_A2DP := true
 BRCM_BT_USE_BTL_IF := true
+WITH_A2DP := true
 
+# EGL
 BOARD_EGL_CFG := device/samsung/captivate/prebuilt/egl.cfg
 
-# Device related defines
+# EMMC storage
+BOARD_EMMC_DEVICE := /dev/block/mmcblk1p1
 
+# Device related defines
 TARGET_PREBUILT_KERNEL := device/samsung/captivate/kernel
 BOARD_KERNEL_CMDLINE := no_console_suspend=1 console=null
 BOARD_KERNEL_BASE := 0x02e00000
 
 BOARD_HAS_NO_SELECT_BUTTON := true
 BOARD_HAS_NO_MISC_PARTITION := true
+BOARD_HAS_NO_RECOVERY_PARTITION := true
 BOARD_USES_FFORMAT := true
-BOARD_RECOVERY_IGNORE_BOOTABLES := true
+#BOARD_RECOVERY_IGNORE_BOOTABLES := true
 
 BOARD_BOOTIMAGE_MAX_SIZE := $(call image-size-from-data-size,0x00280000)
 BOARD_RECOVERYIMAGE_MAX_SIZE := $(call image-size-from-data-size,0x00500000)
@@ -94,10 +110,13 @@ BOARD_SYSTEM_FILESYSTEM_OPTIONS := llw,check=no
 BOARD_CACHE_DEVICE := /dev/block/stl11
 BOARD_CACHE_FILESYSTEM := auto
 BOARD_CACHE_FILESYSTEM_OPTIONS := llw,check=no,nosuid,nodev
-BOARD_SDCARD_DEVICE_PRIMARY := /dev/block/mmcblk0p1
-BOARD_SDCARD_DEVICE_SECONDARY := /dev/block/mmcblk1p1
-BOARD_SDEXT_DEVICE := /dev/block/mmcblk1p2
+#BOARD_SDCARD_DEVICE_PRIMARY := /dev/block/mmcblk0p1
+#BOARD_SDCARD_DEVICE_SECONDARY := /dev/block/mmcblk1p1
+BOARD_SDCARD_INTERNAL_DEVICE := /dev/block/mmcblk0p1
+BOARD_SDCARD_EXTERNAL_DEVICE := /dev/block/mmcblk1p1
+BOARD_SDEXT_FILESYSTEM := auto
+BOARD_HAS_PARAMFS := true
+BOARD_PARAMFS_DEVICE := dev/block/stl3
+BOARD_PARAMFS_FILESYSTEM := rfs
 BOARD_USES_BMLUTILS := true
-
-# For mounting the external MMC
-BOARD_EMMC_DEVICE := /dev/block/mmcblk1p1
+#BOARD_HAS_SMALL_RECOVERY := true
